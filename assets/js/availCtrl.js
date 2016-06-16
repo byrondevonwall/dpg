d1e0f078a9c6832c820fb725976863d982a5f3ef
Byron Wall <byrondevonwall@gmail.com>
2016-06-16 11:57:19 -0400

    add google map and new landing image

--- assets/js/availCtrl.js
+++ assets/js/availCtrl.js
@@ -1,5 +1,24 @@
 dpg.controller('availCtrl', ['$scope', '$location', function($scope, $location){
 
-  
+  var mapOptions = {
+    zoom: 12,
+    center: durham,
+    mapTypeId: google.maps.mapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById('avail-map'), mapOptions);
+
+
+  $scope.changeInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+
+  $scope.addslide = function(){
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '/path/to/img'
+    })
+  }
+
 
 }]);//end of controller
